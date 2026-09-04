5874c59656ba40674b976d0e571d976e548b1653
aiden <aidenpeng0504@gmail.com>
2026-09-04 20:18:29 +0800

    udpate

--- 专题/企业面试题/bytedance.js
+++ 专题/企业面试题/bytedance.js
@@ -191,4 +191,220 @@ var findMinArrowShots = function (points) {
  * @param {number} n
  * @return {number}
  */
-var bulbSwitch = function (n) {}
+var bulbSwitch = function (n) {
+    return Math.floor(Math.sqrt(n))
+}
+
+/**
+ * 3345. 最小可整除数位乘积 I
+ * 给你两个整数 n 和 t 。请你返回大于等于 n 的 最小 整数，且该整数的 各数位之积 能被 t 整除
+ */
+var smallestNumber = function (n, t) {
+    // 输入：n = 10, t = 2 输出：10
+    // 解释： 10 的数位乘积为 0 ，可以被 2 整除，所以它是大于等于 10 且满足题目要求的最小整数。
+
+    // 先计算位积, 能被整除，需要 % t === 0
+    function calc(num) {
+        let s = num + ''
+        let res = 1
+        for (const c of s) {
+            res = res * c
+        }
+        return res
+    }
+
+    let start = n
+    while (calc(start) % t !== 0) {
+        start++
+    }
+
+    return start
+}
+
+/**
+ * 1365. 有多少小于当前数字的数字
+ * 给你一个数组 nums，对于其中每个元素 nums[i]，请你统计数组中比它小的所有数字的数目
+ * 对于每个 nums[i] 你必须计算出有效的 j 的数量，其中 j 满足 j != i 且 nums[j] < nums[i]
+ */
+var smallerNumbersThanCurrent = function (nums) {
+    const len = nums.length
+    const res = Array(len).fill(0)
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (i === j) continue
+
+            if (nums[j] < nums[i]) {
+                res[i]++
+            }
+        }
+    }
+    return res
+
+    // 计数排序
+    const cnt = new Array(101).fill(0) // 因为 num[i]最大为100
+    const len = nums.length
+    for (let i = 0; i < len; i++) {
+        cnt[nums[i]]++ // 将nums[i]纳入 cnt 数组对应位置统计
+    }
+    for (let i = 1; i <= 100; i++) {
+        cnt[i] += cnt[i - 1] // 合并统计每一个比自身小的数
+    }
+
+    const res = []
+    for (let i = 0; i < len; i++) {
+        res.push(nums[i] ? cnt[nums[i] - 1] : 0) // 如果nums[i]==0，那么下标就不能取 nums[i] - 1; 所以要处理下边界
+    }
+    return res
+}
+
+/**
+ * 922. 按奇偶排序数组 II
+ * 给定一个非负整数数组 nums，  nums 中一半整数是 奇数 ，一半整数是 偶数
+ *
+ * 对数组进行排序，以便当 nums[i] 为奇数时，i 也是 奇数 ；当 nums[i] 为偶数时， i 也是 偶数
+ *
+ * 输入：nums = [4,2,5,7] 输出：[4,5,2,7] 解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+ *
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function (nums) {
+    // 也就是奇数下标放的是奇数，偶数下标放的是偶数
+    // const oddTemp = []
+    // const evenTemp = []
+
+    // for (const num of nums) {
+    //     if (num % 2 !== 0) {
+    //         oddTemp.push(num)
+    //     } else {
+    //         evenTemp.push(num)
+    //     }
+    // }
+    // const len = nums.length
+    // const res = []
+    // let evenFlag = true
+    // for (let i = 0; i < len; i++) {
+    //     res.push(evenFlag ? evenTemp.pop() : oddTemp.pop())
+    //     evenFlag = !evenFlag
+    // }
+    // return res
+
+    // let oddIdx = 1,
+    //     evenIdx = 0
+    // const res = []
+    // for (const num of nums) {
+    //     if (num % 2 === 0) {
+    //         res[evenIdx] = num
+    //         evenIdx += 2
+    //     } else {
+    //         res[oddIdx] = num
+    //         oddIdx += 2
+    //     }
+    // }
+    // return res
+
+    const len = nums.length
+    let oddIdx = 1
+    for (let i = 0; i < len; i += 2) {
+        if (nums[i] & 1) {
+            while (nums[oddIdx] & 1) {
+                oddIdx += 1
+            }
+            swap(i, oddIdx)
+        }
+    }
+    function swap(i, j) {
+        ;[nums[i], nums[j]] = [nums[j], nums[i]]
+    }
+    return nums
+}
+
+/**
+ * 2996. 大于等于顺序前缀和的最小缺失整数
+ *
+ * 如果一个前缀 nums[0..i] 满足对于 1 <= j <= i 的所有元素都有 nums[j] = nums[j - 1] + 1 ，那么我们称这个前缀是一个 顺序前缀
+ * 特殊情况是，只包含 nums[0] 的前缀也是一个 顺序前缀 。
+ * 返回 nums 中没有出现过的 最小 整数 x ，满足 x 大于等于 最长 顺序前缀的和
+ *
+ * 输入：nums = [3,4,5,1,12,14,13] 输出：15
+ * 解释：nums 的最长顺序前缀是 [3,4,5] ，和为 12 ，12、13 和 14 都在数组中，但 15 不在，所以 15 是大于等于最长顺序前缀和的最小整数。
+ */
+var missingInteger = function (nums) {
+    /**
+     * 1. 找到最长顺序前缀，得到和
+     * 2. 匹配当前数组中，符合没有出现过的大于该和的最小整数
+     * 3. 下标必须是从0开始
+     */
+    // let maxLen = 1
+    // let sum = nums[0]
+    // let count = nums[0]
+    // let len = 1
+    // const n = nums.length
+    // for (let i = 1; i < n; i++) {
+    //     if (nums[i] === nums[i - 1] + 1) {
+    //         count += nums[i]
+    //         len++
+    //         if (i === len - 1 && len > maxLen) {
+    //             maxLen = len
+    //             sum = count
+    //         }
+    //     } else {
+    //         if (len > maxLen) {
+    //             maxLen = len
+    //             sum = count
+    //         }
+    //         count = nums[i]
+    //         len = 1
+    //     }
+    // }
+    // let overFlag = false
+    // for (const num of nums) {
+    //     if (num >= sum) {
+    //         sum = num
+    //         overFlag = true
+    //     }
+    // }
+    // return overFlag ? sum + 1 : sum
+
+    let total = nums[0]
+    let numSet = new Set(nums)
+
+    const len = nums.length
+    for (let i = 1; i < len; i++) {
+        if (nums[i] === nums[i - 1] + 1) {
+            total += nums[i]
+        } else {
+            break
+        }
+    }
+    while (numSet.has(total)) {
+        total += 1
+    }
+    return total
+}
+
+/**
+ * 1768. 交替合并字符串
+ * 给你两个字符串 word1 和 word2 。请你从 word1 开始，通过交替添加字母来合并字符串
+ * 如果一个字符串比另一个字符串长，就将多出来的字母追加到合并后字符串的末尾
+ * 返回 合并后的字符串
+ *
+ */
+var mergeAlternately = function (word1, word2) {
+    let newStr = ''
+    const len1 = word1.length,
+        len2 = word2.length
+    let i = 0,
+        j = 0
+    while (i < len1 || j < len2) {
+        if (i < len1) {
+            newStr = newStr + word1[i]
+            i++
+        }
+        if (j < len2) {
+            newStr = newStr + word2[j]
+            j++
+        }
+    }
+    return newStr
+}
