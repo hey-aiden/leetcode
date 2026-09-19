951fdd2ed4b97a0ac6c060dcc053898008dc2fbf
aiden <aidenpeng0504@gmail.com>
2026-09-19 17:51:33 +0800

    update

--- 专题/回溯/index.js
+++ 专题/回溯/index.js
@@ -171,3 +171,176 @@ var combinationSum2 = function (candidates, target) {
     trackingBack(0, 0)
     return res
 }
+
+/**
+ * LCR 086. 分割回文串
+ * 给定一个字符串 s ，请将 s 分割成一些子串，使每个子串都是 回文串 ，返回 s 所有可能的分割方案
+ *
+ * 输入：s = "google" 输出：[["g","o","o","g","l","e"],["g","oo","g","l","e"],["goog","l","e"]]
+ */
+var partition = function (s) {
+    const len = s.length
+
+    const res = []
+    let temp = []
+
+    function checkStr(start, end) {
+        while (start <= end) {
+            if (s[start] !== s[end]) return false
+            start++
+            end--
+        }
+        return true
+    }
+    function trackingBack(index) {
+        // 要的是分割方案，所以要index到最后才能收集一次，且要保证内部每一项都是回文子串
+        if (index >= len) {
+            res.push([...temp]) // 结束一轮收集一次
+            return
+        }
+
+        for (let i = index; i < len; i++) {
+            if (checkStr(index, i)) {
+                const str = s.substring(index, i + 1)
+                temp.push(str)
+            } else {
+                continue
+            }
+            trackingBack(i + 1)
+            temp.pop()
+        }
+    }
+
+    trackingBack(0)
+
+    return res
+}
+
+/**
+ * 93. 复原 IP 地址
+ */
+var restoreIpAddresses = function (s) {
+    const len = s.length
+    if (len > 16) return []
+    function checkIp(start, end) {
+        if (end > start && Number(s[start]) === 0) return false
+
+        const splitNum = s.substring(start, end + 1)
+        if (Number(splitNum) > 255) return false
+
+        return true
+    }
+
+    const minLen = Math.floor(len / 4)
+    const res = []
+    const temp = []
+    function trackingBack(start, count) {
+        if (count > 4) return
+
+        if (start >= len && count === 4) {
+            // 遍历到最后一位，且刚好分割4组
+            res.push(temp.join('.'))
+            return
+        }
+
+        for (let i = start; i < len; i += minLen) {
+            if (checkIp(start, i)) {
+                temp.push(s.substring(start, i + 1))
+            } else {
+                continue
+            }
+            trackingBack(i + 1, count + 1)
+            temp.pop()
+        }
+    }
+    trackingBack(0, 0)
+    return res
+}
+
+/**
+ * LCR 087. 复原 IP 地址
+ * 给定一个只包含数字的字符串 s ，用以表示一个 IP 地址，返回所有可能从 s 获得的 有效 IP 地址
+ *
+ * 有效 IP 地址 :  正好由四个整数（每个整数位于 0 到 255 之间组成，且不能含有前导 0），整数之间用 '.' 分隔
+ *
+ * 例如："0.1.2.201" 和 "192.168.1.1" 是 有效 IP 地址，但是 "0.011.255.245"、"192.168.1.312" 和 "192.168@1.1" 是 无效 IP 地址
+ *
+ * 输入：s = "25525511135" 输出：["255.255.11.135","255.255.111.35"]
+ * 输入：s = "0000" 输出：["0.0.0.0"]
+ */
+var restoreIpAddresses = function (s) {
+    const len = s.length
+
+    if (len > 16) return []
+
+    const res = []
+    const temp = []
+
+    function checkIp(start, end) {
+        const prefixNum = Number(s[start])
+
+        // 说明是前导0，排除
+        if (end > start && prefixNum === 0) return false
+
+        const extraNum = s.substring(start, end + 1)
+        if (Number(extraNum) > 255) return false
+        return true
+    }
+
+    const minStep = Math.floor(len / 4)
+
+    function trackingBack(start, count) {
+        if (count > 4) return
+        if (start >= len && count === 4) {
+            res.push(temp.join('.'))
+            return
+        }
+
+        for (let i = start; i < len; i++) {
+            if (checkIp(start, i)) {
+                const char = s.substring(start, i + 1)
+                temp.push(char)
+            } else {
+                continue
+            }
+            trackingBack(i + 1, count + 1)
+            temp.pop()
+        }
+    }
+    trackingBack(0, 0)
+    return res
+}
+
+/**
+ * 78. 子集
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集
+ */
+var subsets = function (nums) {}
+
+/**
+ * 90. 子集 II
+ * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的 子集（幂集）
+ *
+ * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+ *
+ * 输入：nums = [1,2,2] 输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+ */
+var subsetsWithDup = function (nums) {
+    const len = nums.length
+
+    const res = []
+    const temp = []
+
+    function trackingBack(start) {
+        res.push([...temp])
+        for (let i = start; i < len; i++) {
+            temp.push(nums[i])
+            trackingBack(i + 1)
+            temp.pop()
+        }
+    }
+    trackingBack(0)
+
+    return res
+}
